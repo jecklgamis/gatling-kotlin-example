caad6a752763529c60f6ad9a1ca40d8b939d7575
Jerrico Gamis <jecklgamis@gmail.com>
2025-07-16 08:10:30 +1000

    Upgrade to gatling-3.14.3; Fix base image, runner scripts

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 SIMULATION_NAME=${SIMULATION_NAME:-gatling.test.example.simulation.ExampleSimulation}
+JAVA_OPTS="${JAVA_OPTS} --add-opens java.base/java.lang=ALL-UNNAMED"
 exec java ${JAVA_OPTS} -cp bin/gatling-kotlin-example.jar  io.gatling.app.Gatling --simulation ${SIMULATION_NAME} --results-folder results
--- run-simulation-using-jar.sh
+++ run-simulation-using-jar.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 JAVA_OPTS="-DbaseUrl=http://localhost:8080  -DdurationMin=1 -DrequestPerSecond=10"
 SIMULATION_NAME=gatling.test.example.simulation.ExampleSimulation
+JAVA_OPTS="${JAVA_OPTS} --add-opens java.base/java.lang=ALL-UNNAMED"
 java ${JAVA_OPTS} -cp target/gatling-kotlin-example.jar io.gatling.app.Gatling --simulation "${SIMULATION_NAME}" --results-folder results
